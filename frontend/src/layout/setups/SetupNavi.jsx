e962cd77e1b5dcbb5bac05753c8611d957d9de7f
徐涛 <midnite@outlook.com>
2023-07-22 06:08:04 +0800

    enhance(setup):调整界面可用功能。

--- frontend/src/layout/setups/SetupNavi.jsx
+++ frontend/src/layout/setups/SetupNavi.jsx
@@ -3,8 +3,8 @@ import { NavLink, Stack } from '@mantine/core';
 export function SetupNavigation() {
   return (
     <Stack spacing="sm">
-      <NavLink label="应用配置" />
-      <NavLink label="Civitai API 配置" />
+      <NavLink label="应用配置" disabled />
+      <NavLink label="Civitai API 配置" disabled />
       <NavLink label="网络代理配置" />
       <NavLink label="Stable Diffusion WebUI 配置" />
       <NavLink label="ComfyUI 配置" />
