c79aedd26147d934ddfb442977094d2ed3430340
徐涛 <midnite@outlook.com>
2023-07-23 08:41:50 +0800

    build(vite):定义路径别名。

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,25 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+function _resolve(dir: string) {
+  return path.resolve(__dirname, dir);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': _resolve('src'),
+      '@wails': _resolve('wailsjs')
+    }
+  },
   plugins: [
     react({
-      jsxImportSource: "@emotion/react",
+      jsxImportSource: '@emotion/react',
       babel: {
-        plugins: ["@emotion/babel-plugin"],
-      },
-    }),
-  ],
+        plugins: ['@emotion/babel-plugin']
+      }
+    })
+  ]
 });
