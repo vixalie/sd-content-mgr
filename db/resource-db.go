ca370fab310dd500d3c0514060d7e78c31e90b87
徐涛 <midnite@outlook.com>
2023-07-27 14:00:38 +0800

    fix(db):数据库连接实例恢复保存在Context中的形式。

--- db/resource-db.go
+++ db/resource-db.go
@@ -2,11 +2,9 @@ package db
 
 import (
 	"context"
-	"os"
 
 	"github.com/glebarez/sqlite"
 	"github.com/vixalie/sd-content-manager/entities"
-	"github.com/wailsapp/wails/v2/pkg/runtime"
 	"gorm.io/gorm"
 )
 
@@ -14,21 +12,12 @@ type dbConnection string
 
 const DBConnection dbConnection = "db"
 
-var CacheDB *gorm.DB
-
-func init() {
+func InitDB(ctx *context.Context) error {
 	CacheDB, err := gorm.Open(sqlite.Open("sdres.db"), &gorm.Config{
 		DisableForeignKeyConstraintWhenMigrating: true,
 	})
 	if err != nil {
-		runtime.MessageDialog(context.TODO(), runtime.MessageDialogOptions{
-			Type:    runtime.ErrorDialog,
-			Title:   "初始化错误",
-			Message: "数据库创建失败",
-			Buttons: []string{"OK"},
-		})
-		println("Error:", err.Error())
-		os.Exit(1)
+		return err
 	}
 	CacheDB.AutoMigrate(
 		&entities.Model{},
@@ -38,4 +27,6 @@ func init() {
 		&entities.Image{},
 		&entities.FileCache{},
 	)
+	*ctx = context.WithValue(*ctx, DBConnection, CacheDB)
+	return nil
 }
--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"embed"
+	"os"
 
 	"archgrid.xyz/ag/toolsbox/serial_code/hail"
 	"github.com/vixalie/sd-content-manager/config"
+	"github.com/vixalie/sd-content-manager/db"
 	"github.com/vixalie/sd-content-manager/models"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 	"golang.org/x/net/context"
 )
 
@@ -35,6 +38,17 @@ func main() {
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup: func(ctx context.Context) {
 			hail.Initialize(1)
+			err := db.InitDB(&ctx)
+			if err != nil {
+				runtime.MessageDialog(ctx, runtime.MessageDialogOptions{
+					Type:    runtime.ErrorDialog,
+					Title:   "初始化错误",
+					Message: "数据库创建失败",
+					Buttons: []string{"OK"},
+				})
+				println("Error:", err.Error())
+				os.Exit(1)
+			}
 			app.startup(ctx)
 			settings.SetContext(ctx)
 			modelController.SetContext(ctx)
