79b05c212385d8c6bf28c23ea1a0492474df83c5
徐涛 <midnite@outlook.com>
2023-07-31 23:05:01 +0800

    enhance(model):删除不必要的内容调试语句。

--- models/file-info.go
+++ models/file-info.go
@@ -3,7 +3,6 @@ package models
 import (
 	"context"
 	"errors"
-	"fmt"
 
 	"github.com/vixalie/sd-content-manager/db"
 	"github.com/vixalie/sd-content-manager/entities"
@@ -20,10 +19,7 @@ func fetchUncachedFileInfo(ctx context.Context, fileId string) (*entities.FileCa
 func fetchCachedModelInfo(ctx context.Context, modelVersionId int) (*entities.ModelVersion, error) {
 	dbConn := ctx.Value(db.DBConnection).(*gorm.DB)
 	var modelVersion entities.ModelVersion
-	fmt.Printf("modelVersionId: %v\n", modelVersionId)
 	result := dbConn.Joins("Model").Preload("Covers").First(&modelVersion, modelVersionId)
-	fmt.Printf("Result: %v\n", result)
-	fmt.Printf("modelVersion: %v\n", modelVersion)
 	if result.Error != nil {
 		return nil, errors.New("指定获取的已缓存模型信息实际为未缓存模型")
 	}
