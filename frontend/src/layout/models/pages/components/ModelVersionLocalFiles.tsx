e8d721939b9f4d399e5f5100d530326f31c8b18a
徐涛 <midnite@outlook.com>
2023-08-02 09:44:34 +0800

    refactor(suspend):更换用于激活Suspend组件的支持库。

--- frontend/src/layout/models/pages/components/ModelVersionLocalFiles.tsx
+++ frontend/src/layout/models/pages/components/ModelVersionLocalFiles.tsx
@@ -3,14 +3,14 @@ import { Stack } from '@mantine/core';
 import { FetchModelLocalFiles } from '@wails/go/models/ModelController';
 import { nanoid } from 'nanoid';
 import { FC } from 'react';
-import usePromise from 'react-promise-suspense';
+import { suspend } from 'suspend-react';
 
 type ModelVersionLocalFilesProps = {
   versionId: number;
 };
 
 export const ModelVersionLocalFiles: FC<ModelVersionLocalFilesProps> = ({ versionId }) => {
-  const files = usePromise(FetchModelLocalFiles, [versionId]);
+  const files = suspend(FetchModelLocalFiles, [versionId]);
   return (
     <Stack spacing="sm">
       {files.map(file => (
