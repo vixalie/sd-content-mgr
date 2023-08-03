00b9142c1ece1acfc19cd530e0f4e562d3f83195
徐涛 <midnite@outlook.com>
2023-08-03 23:09:23 +0800

    fix(filename):修正一处在修改模型文件名的时候可能存在无更新的问题。

--- frontend/src/components/RenameableFile.tsx
+++ frontend/src/components/RenameableFile.tsx
@@ -35,25 +35,28 @@ export const RenameableFile: FC<RenameableFileProps> = ({
   const [extPart, setExtPart] = useState<string>('');
   const ref = useRef<TextInput>(null);
   const revalidator = useRevalidator();
-  const switchEditState = useCallback(async (state: boolean) => {
-    setEditing(state);
-    if (state) {
-      try {
-        const [name, ext] = await BreakModelFileParts(fileId);
-        setNamePart(name);
-        setExtPart(ext);
-      } catch (e) {
-        console.error('[error]重命名文件：', e);
-        notifications.show({
-          title: '重命名文件失败',
-          message: `未能成功获取文件名的分解，${e.message}`,
-          autoClose: 5000,
-          color: 'red',
-          withCloseButton: false
-        });
+  const switchEditState = useCallback(
+    async (state: boolean) => {
+      setEditing(state);
+      if (state) {
+        try {
+          const [name, ext] = await BreakModelFileParts(fileId);
+          setNamePart(name);
+          setExtPart(ext);
+        } catch (e) {
+          console.error('[error]重命名文件：', e);
+          notifications.show({
+            title: '重命名文件失败',
+            message: `未能成功获取文件名的分解，${e.message}`,
+            autoClose: 5000,
+            color: 'red',
+            withCloseButton: false
+          });
+        }
       }
-    }
-  }, []);
+    },
+    [fileId]
+  );
   const renameFile = useCallback(async () => {
     try {
       await RenameModelFile(fileId, namePart);
