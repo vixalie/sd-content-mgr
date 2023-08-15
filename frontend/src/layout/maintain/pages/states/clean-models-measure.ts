722181c25bd9377b92b8f7053a4ee2efef87a60a
徐涛 <midnite@outlook.com>
2023-08-15 21:35:02 +0800

    refactor(type):移动定义类型初始值的位置。

--- frontend/src/layout/maintain/pages/states/clean-models-measure.ts
+++ frontend/src/layout/maintain/pages/states/clean-models-measure.ts
@@ -1,17 +1,6 @@
 import { createStore } from '@/store_creator';
-import type { ElementPosition } from '@/types';
+import { InitialElementPosition, type ElementPosition } from '@/types';
 import { clone } from 'ramda';
-
-const InitialElementPosition: ElementPosition = {
-  x: 0,
-  y: 0,
-  top: 0,
-  left: 0,
-  bottom: 0,
-  right: 0,
-  width: 0,
-  height: 0
-};
 
 type CleanModelsMeasureState = {
   page: ElementPosition;
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -12,3 +12,14 @@ export type ElementPosition = {
   width: number;
   height: number;
 };
+
+export const InitialElementPosition: ElementPosition = {
+  x: 0,
+  y: 0,
+  top: 0,
+  left: 0,
+  bottom: 0,
+  right: 0,
+  width: 0,
+  height: 0
+};
