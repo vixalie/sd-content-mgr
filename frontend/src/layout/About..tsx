d325d82e7eed68bc76b2c67e0c593d654ef0e655
徐涛 <midnite@outlook.com>
2023-09-03 16:57:12 +0800

    doc(readme):调整说明文件。

--- frontend/src/layout/About..tsx
+++ frontend/src/layout/About..tsx
@@ -16,7 +16,7 @@ export function About() {
         <h1>SD Content Manager</h1>
       </Box>
       <Box py={8}>
-        <Text>Version 0.4.12</Text>
+        <Text>Version 0.4.15</Text>
       </Box>
     </Flex>
   );
